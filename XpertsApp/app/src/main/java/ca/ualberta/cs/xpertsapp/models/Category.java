4aa2d1aa1a5f50aab39d4a4890e49de43ca873e1
jstuehmer <stuehmer@ualberta.ca>
2015-10-16 17:20:36 -0600

    Added Category and User

---
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/models/Category.java
@@ -0,0 +1,4 @@
+package ca.ualberta.cs.xpertsapp.models;
+
+public class Category {
+}
--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/models/Service.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/models/Service.java
@@ -1,6 +1,24 @@
 package ca.ualberta.cs.xpertsapp.models;
 
+import java.util.ArrayList;
+
 public class Service {
+
+    private String name;
+    private String description;
+    private Category category;
+    private ArrayList<pics> photos;
+    private boolean shareable;
+    private User owner;
+
+    public Service(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 
 
 }
