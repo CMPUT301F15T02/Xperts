d742f1ee0dc3ff32b0d44c09870d78cf5ee70fb2
SeannMurdock <murdock@ualberta.ca>
2015-10-16 17:17:09 -0600

    Added Category, Trade, TradeState, User

---
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/models/Category.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.xpertsapp.models;
+
+/**
+ * Created by murdock on 10/16/15.
+ */
+public enum Category {
+    ACCOUNTING, CONSTRUCTION, COMPUTER, FITNESS,
+    GENERAL_LABOUR, HEALTHCARE, LEGAL, MUSIC,
+    TUTORING, OTHER
+}
---
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/models/TradeState.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.xpertsapp.models;
+
+/**
+ * Created by murdock on 10/16/15.
+ */
+public enum TradeState {
+   PENDING,ACCPETED,DECLINED,CANCELLED
+}
