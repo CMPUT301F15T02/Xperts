fb7d267fa6dc40813a8a46d57a127b1f061359f2
kathb <kzb1895@gmail.com>
2015-10-29 13:51:27 -0600

    Added methods to Trade class, test for TradeTest

--- XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/UnitTests/TradeTest.java
+++ XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/UnitTests/TradeTest.java
@@ -2,10 +2,23 @@ package ca.ualberta.cs.xpertsapp.UnitTests;
 
 import junit.framework.TestCase;
 
+import ca.ualberta.cs.xpertsapp.models.Trade;
+import ca.ualberta.cs.xpertsapp.models.User;
+
 /**
  * Created by hammadjutt on 2015-10-29.
  */
 public class TradeTest extends TestCase {
+
+    public void testTrade() {
+        String id = "1";
+        Boolean isCounter = Boolean.FALSE;
+        Trade trade = new Trade(id, isCounter);
+        assertTrue("id is not equal", id.equals(trade.getId()));
+        assertTrue("isCounter not equal", isCounter.equals(trade.isCounterOffer()));
+        //test getOwner()
+
+    }
 
     public void setUp() throws Exception {
         super.setUp();
--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/models/Trade.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/models/Trade.java
@@ -5,11 +5,72 @@ import java.util.Date;
 
 
 public class Trade {
-    private int id;
+    private String id;
     private ArrayList<Service> borrowerServices = new ArrayList<Service>();
     private ArrayList<Service> ownerServices = new ArrayList<Service>();
     private Date start;
     private Date updated;
     private Boolean isCounter;
     private TradeState status;
+
+    public Trade(String id, Boolean isCounter) {
+        this.id = id;
+        this.isCounter = isCounter;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public User getOwner() {
+        return null;
+    }
+
+    public User getBorrower() {
+        return null;
+    }
+
+    public ArrayList<Service> getOwnerServices() {
+        return null;
+    }
+
+    public void addOwnerService(Service service) {
+    }
+
+    public void removeOwnerService(Service service) {
+
+    }
+
+    public ArrayList<Service> getBorrowerServices() {
+        return null;
+    }
+
+    public void addBorrowerService(Service service) {
+
+    }
+
+    public void removeBorrowerService(Service service) {
+
+    }
+
+    public Date getProposedDate() {
+        return null;
+    }
+
+    public Date getLastUpdatedDate() {
+        return null;
+    }
+
+    public Boolean isCounterOffer() {
+        return isCounter;
+    }
+    /*
+    public TradeStatus getStatus() {
+        return null;
+    }*/
+
+    private Boolean isEditable() {
+        return null;
+    }
+
 }
