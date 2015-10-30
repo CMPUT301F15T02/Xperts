47f73172b12168ef6ad53da55a4c56057c7573d2
lucahuy <huybi.truong@live.com>
2015-10-30 17:10:54 -0600

    commit before pull

--- XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/ConfigurationTests/EditProfileTest.java
+++ XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/ConfigurationTests/EditProfileTest.java
@@ -1,25 +1,54 @@
 package ca.ualberta.cs.xpertsapp.ConfigurationTests;
 
+import android.app.Activity;
 import android.app.Application;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
+import android.widget.Button;
 
 import ca.ualberta.cs.xpertsapp.models.User;
 import ca.ualberta.cs.xpertsapp.views.MainActivity;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.Before;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class EditProfileTest extends ActivityInstrumentationTestCase2 {
+public class EditProfileTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private MainActivity mActivity;
+
     public EditProfileTest() {
         super(MainActivity.class);
     }
 
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        mActivity = getActivity();
+    }
+
     public void testProfileChanged() {
-        /*User user = new User();
-        profileWasChanged = Boolean.FALSE;
-        user.addObserver(this);
+        User user = new User();
+        //profileWasChanged = Boolean.FALSE;
+        //user.addObserver(this);
         user.setName("test");
-        assertTrue(profileWasChanged);*/
+        //assertTrue(profileWasChanged);*/
+
+        onView(withId(R.id.editTextName))
+                .perform(typeText("newName"), closeSoftKeyboard());
+        onView(withId(R.id.buttonChangeName)).perform(click());
+
+        // Check that user's name was changed
+        assertEquals("newName", user.getName());
     }
 }
--- XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/ConfigurationTests/ToggleImageDownload.java
+++ XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/ConfigurationTests/ToggleImageDownload.java
@@ -1,13 +1,35 @@
 package ca.ualberta.cs.xpertsapp.ConfigurationTests;
 
 import android.app.Application;
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
+
+import ca.ualberta.cs.xpertsapp.models.User;
+import ca.ualberta.cs.xpertsapp.views.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ToggleImageDownload extends ApplicationTestCase<Application> {
+public class ToggleImageDownload extends ActivityInstrumentationTestCase2 {
     public ToggleImageDownload() {
-        super(Application.class);
+        super(MainActivity.class);
+    }
+
+    public void testToggleDownload() {
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+
+
+        User user = new User();
+        onView(withId(R.id.toggleDownload))
+                .perform(swipeLeft());
+        assertTrue(user.getSettings().getToggleDownload());
     }
 }
