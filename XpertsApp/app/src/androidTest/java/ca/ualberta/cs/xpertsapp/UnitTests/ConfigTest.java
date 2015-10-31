e1865a253b0efe79f817100af9def4d4bc9051ef
David Skrundz <david.skrundz@gmail.com>
2015-10-31 16:03:03 -0600

    all of the test methods are added and failing

---
+++ XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/UnitTests/ConfigTest.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.xpertsapp.UnitTests;
+
+import junit.framework.TestCase;
+
+public class ConfigTest extends TestCase {
+	public void test_10_01_01() {
+		// TODO: Test toggle image download
+		assertTrue(false);
+	}
+
+	public void test_10_02_01() {
+		// TODO: Test edit profile
+		assertTrue(false);
+	}
+}
---
+++ XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/UnitTests/OfflineTest.java
@@ -0,0 +1,20 @@
+package ca.ualberta.cs.xpertsapp.UnitTests;
+
+import junit.framework.TestCase;
+
+public class OfflineTest extends TestCase {
+	public void test_09_01_01() {
+		// TODO: Test add service while offline and push while online
+		assertTrue(false);
+	}
+
+	public void test_09_02_01() {
+		// TODO: Test make trade while offline and push while online
+		assertTrue(false);
+	}
+
+	public void test_09_03_01() {
+		// TODO: Test cache friends and servies for offline use
+		assertTrue(false);
+	}
+}
