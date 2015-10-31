7f15ea93ffd3e6eac6155e55c4ee6ecbdf6978b8
Justin <stuehmer@ualberta.ca>
2015-10-31 15:15:42 -0600

    Commit

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/ServiceManager.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/ServiceManager.java
@@ -1,14 +1,29 @@
 package ca.ualberta.cs.xpertsapp.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import ca.ualberta.cs.xpertsapp.interfaces.IObservable;
 import ca.ualberta.cs.xpertsapp.interfaces.IObserver;
 
 public class ServiceManager implements IObserver {
 
+	private Map<String, Service> services = new HashMap<String, Service>();
+
+	public List<Service> getServices() {
+		return null;
+	}
+
 	// Singleton
 	private static ServiceManager instance = new ServiceManager();
 
-	private ServiceManager() {}
+	private ServiceManager() {
+	}
+
+	public static ServiceManager sharedManager() {
+		return ServiceManager.instance;
+	}
 
 	/// IObserver
 	@Override
