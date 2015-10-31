c33bcd88c3c16448e6d3ce7445b90ce1aedd7dcf
Justin <stuehmer@ualberta.ca>
2015-10-31 13:42:07 -0600

    Edited Service class

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/Service.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/Service.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cs.xpertsapp.model;
 
+import android.graphics.Bitmap;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,18 +9,62 @@ import ca.ualberta.cs.xpertsapp.interfaces.IObservable;
 import ca.ualberta.cs.xpertsapp.interfaces.IObserver;
 
 public class Service implements IObservable {
-	/// Observable
+
+	private String id;
+	private String name = "";
+	private String description = "";
+	private String category = "";
+	private List<Bitmap> pictures;
+	private boolean shareable = true;
+	private String owner = "";
+
+	Service(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setNam
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public List<Bitmap> getPictures() {
+		return pictures;
+	}
+
+	public boolean isShareable() {
+		return shareable;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
 
 	private List<IObserver> observers = new ArrayList<IObserver>();
 
+	@Override
 	public void addObserver(IObserver observer) {
 		this.observers.add(observer);
 	}
 
+	@Override
 	public void removeObserver(IObserver observer) {
 		this.observers.remove(observer);
 	}
 
+	@Override
 	public void notifyObservers() {
 		for (IObserver observer : this.observers) {
 			observer.notify(this);
