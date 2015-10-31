dc7c86c0bb153cfba72897e31e481e0878015ccf
David Skrundz <david.skrundz@gmail.com>
2015-10-31 15:00:04 -0600

    started some test stuff

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/UserManager.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/UserManager.java
@@ -1,5 +1,11 @@
 package ca.ualberta.cs.xpertsapp.model;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,6 +13,7 @@ import java.util.Map;
 
 import ca.ualberta.cs.xpertsapp.interfaces.IObservable;
 import ca.ualberta.cs.xpertsapp.interfaces.IObserver;
+import ca.ualberta.cs.xpertsapp.model.es.SearchHit;
 
 public class UserManager implements IObserver {
 	private Map<String, User> users = new HashMap<String, User>();
@@ -20,13 +27,36 @@ public class UserManager implements IObserver {
 		return users;
 	}
 
+	private void addUser(User user) {
+		user.addObserver(this);
+		this.users.put(user.getID(), user);
+	}
+
 	public User getUser(String id) {
 		// If we have the user loaded
 		if (this.users.containsKey(id)) {
 			return this.users.get(id);
 		}
 		// TODO:
-		return null;
+		try {
+			String loadedData = IOManager.sharedManager().fetchData(Constants.serverUserExtension() + id);
+			SearchHit<User> loadedUser = (new Gson()).fromJson(loadedData, new TypeToken<SearchHit<User>>() {
+			}.getType());
+			if (loadedUser.isFound()) {
+				this.addUser(loadedUser.getSource());
+				return loadedUser.getSource();
+			} else {
+				User newUser = new User(Constants.deviceUUID());
+				this.addUser(newUser);
+				return newUser;
+			}
+		} catch (JsonIOException e) {
+			throw new RuntimeException(e);
+		} catch (JsonSyntaxException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalStateException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public User localUser() {
