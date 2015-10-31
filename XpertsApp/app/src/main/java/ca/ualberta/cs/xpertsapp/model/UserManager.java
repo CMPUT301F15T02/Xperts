e834fe5ac064361440b508335cb80f1d12c5dd47
David Skrundz <david.skrundz@gmail.com>
2015-10-31 13:41:09 -0600

    Implemented User, UserManager(1/2), and IOManager(1/2)

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/UserManager.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/UserManager.java
@@ -1,8 +1,5 @@
 package ca.ualberta.cs.xpertsapp.model;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,23 +7,11 @@ import java.util.Map;
 
 import ca.ualberta.cs.xpertsapp.interfaces.IObservable;
 import ca.ualberta.cs.xpertsapp.interfaces.IObserver;
-import ca.ualberta.cs.xpertsapp.model.es.SearchHit;
 
 public class UserManager implements IObserver {
-	public static UserManager instance = new UserManager();
-
-	private UserManager() {
-	}
-
-	/**
-	 * @return The singleton instance of UserManager
-	 */
-	public static UserManager sharedManager() {
-		return UserManager.instance;
-	}
-
 	private Map<String, User> users = new HashMap<String, User>();
 
+	// Get/Set
 	public List<User> getUsers() {
 		List<User> users = new ArrayList<User>();
 		for (User user : this.users.values()) {
@@ -35,46 +20,42 @@ public class UserManager implements IObserver {
 		return users;
 	}
 
-	private void addUser(User user) {
-		user.addObserver(this);
-		this.users.put(user.getID(), user);
+	public User getUser(String id) {
+		// If we have the user loaded
+		if (this.users.containsKey(id)) {
+			return this.users.get(id);
+		}
+		// TODO:
+		return null;
 	}
 
 	public User localUser() {
 		return this.getUser(Constants.deviceUUID());
 	}
 
-	public User getUser(String id) {
-		if (this.users.containsKey(id)) {
-			return this.users.get(id);
-		}
-		String loadedData = IOManager.sharedManager().fetchData(Constants.serverUserExtension() + id);
-		try {
-			SearchHit<User> loadedUser = (new Gson()).fromJson(loadedData, new TypeToken<SearchHit<User>>() {
-			}.getType());
-			if (loadedUser.isFound()) {
-				this.addUser(loadedUser.getSource());
-				return loadedUser.getSource();
-			} else {
-				User newUser = new User(Constants.deviceUUID(), "", "");
-				this.addUser(newUser);
-				return newUser;
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+	public List<User> findUsers(String meta) {
+		// TODO:
+		return null;
 	}
 
 	public void clearCache() {
 		this.users.clear();
-		// Make sure to reload the local user
 		this.localUser();
 	}
 
-	/// Observer
+	// Singleton
+	private static UserManager instance = new UserManager();
 
+	private UserManager() {
+	}
+
+	public UserManager sharedManager() {
+		return UserManager.instance;
+	}
+
+	// IObserver
+	@Override
 	public void notify(IObservable observable) {
-		User user = (User) observable;
-		IOManager.sharedManager().storeData((new Gson()).toJson(user), Constants.serverUserExtension());
+		// TODO:
 	}
 }
