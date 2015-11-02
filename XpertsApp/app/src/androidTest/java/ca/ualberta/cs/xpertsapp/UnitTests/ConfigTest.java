246aa2e7f3b7bca02fb6c5d975ae4aaa2fbbd46c
David Skrundz <david.skrundz@gmail.com>
2015-11-01 17:23:34 -0700

    another test passing

--- XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/UnitTests/ConfigTest.java
+++ XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/UnitTests/ConfigTest.java
@@ -1,5 +1,10 @@
 package ca.ualberta.cs.xpertsapp.UnitTests;
 
+import ca.ualberta.cs.xpertsapp.model.Constants;
+import ca.ualberta.cs.xpertsapp.model.IOManager;
+import ca.ualberta.cs.xpertsapp.model.User;
+import ca.ualberta.cs.xpertsapp.model.UserManager;
+
 public class ConfigTest extends TestCase {
 	public ConfigTest() {
 		super();
@@ -12,6 +17,8 @@ public class ConfigTest extends TestCase {
 
 	@Override
 	protected void tearDown() throws Exception {
+		IOManager.sharedManager().deleteData(Constants.serverUserExtension() + UserManager.sharedManager().localUser().getID());
+
 		super.tearDown();
 	}
 
@@ -21,7 +28,20 @@ public class ConfigTest extends TestCase {
 	}
 
 	public void test_10_02_01() {
-		// TODO: Test edit profile
-		assertTrue(false);
+		// Test edit profile
+		User user = UserManager.sharedManager().localUser();
+
+		String newName = "Some user's name";
+		String newLocation = "canada";
+		String newContact = "2390832490838042";
+		user.setName(newName);
+		user.setLocation(newLocation);
+		user.setContactInfo(newContact);
+
+		UserManager.sharedManager().clearCache();
+		user = UserManager.sharedManager().localUser();
+		assertEquals(user.getName(), newName);
+		assertEquals(user.getLocation(), newLocation);
+		assertEquals(user.getContactInfo(), newContact);
 	}
 }
