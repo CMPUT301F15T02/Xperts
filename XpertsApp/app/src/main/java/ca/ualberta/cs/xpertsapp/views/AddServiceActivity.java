327d5ee09ee66be124e4175fa22b16f42a4777d7
SeannMurdock <murdock@ualberta.ca>
2015-11-03 13:42:59 -0700

    made the spinner work in AddServiceActivity

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/views/AddServiceActivity.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/views/AddServiceActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import ca.ualberta.cs.xpertsapp.R;
 import ca.ualberta.cs.xpertsapp.model.CategoryList;
+import ca.ualberta.cs.xpertsapp.model.Category;
 import ca.ualberta.cs.xpertsapp.model.Service;
 
 
@@ -23,10 +25,12 @@ public class AddServiceActivity extends Activity {
 		setContentView(R.layout.activity_add_service);
 		EditText Title =(EditText) findViewById(R.id.editText);
 		EditText Description = (EditText) findViewById(R.id.editText2);
-		Spinner Category = (Spinner) findViewById(R.id.spinner);
+		Spinner Categories = (Spinner) findViewById(R.id.spinner);
 		CheckBox Private = (CheckBox) findViewById(R.id.checkBox);
 		CategoryList CL = CategoryList.sharedCategoryList();
 		//Category.setAdapter();
+		ArrayAdapter<Category>  categoryarrayadapter = new ArrayAdapter<Category>(this, android.R.layout.simple_spinner_dropdown_item,CL.getCategories());
+		Categories.setAdapter(categoryarrayadapter);
 
 	}
 
