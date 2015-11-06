e5ad64fad54d5a20625b4caa58a82d61aebdf5c9
kathb <kzb1895@gmail.com>
2015-11-05 21:09:55 -0700

    added a bit of javadoc

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/controllers/ProfileController.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/controllers/ProfileController.java
@@ -9,10 +9,15 @@ import ca.ualberta.cs.xpertsapp.model.UserManager;
 
 /**
  * Created by kmbaker on 11/3/15.
+ * Controller for adding and removing friends
  */
 public class ProfileController {
     //adds friend to local user unless user doesn't exist, then returns null
     //should probably be an exception
+    /**
+     * @param email the email of the user to add
+     * @return the users added or null if user not found
+     */
     public User addFriend(String email){
         User user = MyApplication.getLocalUser();
         User friend;
@@ -28,7 +33,10 @@ public class ProfileController {
         friend.addFriend(user);
         return friend;
     }
-    //deletes friend from local user
+    /**
+     * Deletes friend from local user
+     * @param friend the user to delete
+     */
     public void deleteFriend(User friend) {
         User user = MyApplication.getLocalUser();
         user.removeFriend(friend);
