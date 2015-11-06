5070d8cb93f29b4bac04d8bf864e73dd7768ec74
kathb <kzb1895@gmail.com>
2015-11-05 21:30:02 -0700

    edit profile works, but didn't do edit email

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/controllers/EditProfileController.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/controllers/EditProfileController.java
@@ -2,9 +2,15 @@ package ca.ualberta.cs.xpertsapp.controllers;
 
 import android.widget.EditText;
 
+import ca.ualberta.cs.xpertsapp.MyApplication;
+import ca.ualberta.cs.xpertsapp.model.User;
+
 public class EditProfileController {
 
+	//can you edit email?
 	public void editProfile(EditText email, EditText name, EditText location) {
-
+		User user = MyApplication.getLocalUser();
+		user.setName(name.getText().toString());
+		user.setLocation(location.getText().toString());
 	}
 }
