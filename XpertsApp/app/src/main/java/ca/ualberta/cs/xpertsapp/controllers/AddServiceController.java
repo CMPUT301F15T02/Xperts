77299a1e18f0ecb7636450e923715529d629bda7
SeannMurdock <murdock@ualberta.ca>
2015-11-17 13:47:12 -0700

    added javadoc for edit profile controller

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/controllers/AddServiceController.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/controllers/AddServiceController.java
@@ -18,16 +18,16 @@ import ca.ualberta.cs.xpertsapp.model.UserManager;
 import ca.ualberta.cs.xpertsapp.views.AddServiceActivity;
 
 /**
- * This controller communicates with the addService activity and the model
+ * This controller communicates with  {@link AddServiceActivity}  and the model
  */
 public class AddServiceController {
 
     /**
      * This function takes the inputted information and converts it into a Service object in the database
-     * @param title this is the string value of the Service's title passed as a EditText from the addService activity, there are no character restrictions. Ex "Write JavaDoc"
-     * @param description this is a description of the service passed as an EditText from the addService activity that a user writes to better describe the service to other users. Ex "I write excellent JavaDoc for java projects. This includes descriptions and examples of different variables and functions. And I don't forget about runime."
+     * @param title this is the string value of the Service's title passed as a EditText from the {@link AddServiceActivity} there are no character restrictions. Ex "Write JavaDoc"
+     * @param description this is a description of the service passed as an EditText from the  {@link AddServiceActivity}  that a user writes to better describe the service to other users. Ex "I write excellent JavaDoc for java projects. This includes descriptions and examples of different variables and functions. And I don't forget about runime."
      * @param category is the category of a Service. Category objects are used by User's to better focus their search results.Category objects have a string title. Ex "Computers"
-     * @param isPrivate is the setting that allows a user to keep a Service unsearchable by other Users. It is passed as a CheckBox from the addService activity. For example User Bob might want to keep his "Write JavaDoc" private because it is highly in demand. This way he isn't spammed by 301 students but is still able to offer his service to other User's who have a service that he wants.
+     * @param isPrivate is the setting that allows a user to keep a Service unsearchable by other Users. It is passed as a CheckBox from  {@link AddServiceActivity} . For example User Bob might want to keep his "Write JavaDoc" private because it is highly in demand. This way he isn't spammed by 301 students but is still able to offer his service to other User's who have a service that he wants.
      */
      public void addService(EditText title, EditText description, Category category,CheckBox isPrivate) {
 
@@ -40,7 +40,7 @@ public class AddServiceController {
     }
 
     /**
-     *This function takes the inputted information from the add service activity and converts it into a service object in the database
+     *This function takes the inputted information from the  {@link AddServiceActivity}  and converts it into a service object in the database
      * @param title see above
      * @param description "" ""
      * @param category "" ""
