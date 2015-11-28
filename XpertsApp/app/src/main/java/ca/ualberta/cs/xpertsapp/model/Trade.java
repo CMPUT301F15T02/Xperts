f45a38dddaed558ed1532d509c8bfcd1c149bc62
David Skrundz <david.skrundz@gmail.com>
2015-11-27 17:00:42 -0700

    tests are passing

--- XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/Trade.java
+++ XpertsApp/app/src/main/java/ca/ualberta/cs/xpertsapp/model/Trade.java
@@ -85,7 +85,7 @@ public class Trade implements IObservable {
 	 */
 	public void addOwnerService(Service service) {
 		if (!this.isEditable()) throw new AssertionError();
-		if (service.getOwner() != this.getOwner()) throw new AssertionError();
+		if (service.getOwner() != this.getBorrower()) throw new AssertionError();
 		this.ownerServices.add(service.getID());
 		this.notifyObservers();
 	}
@@ -96,7 +96,7 @@ public class Trade implements IObservable {
 	 */
 	public void removeOwnerService(Service service) {
 		if (!this.isEditable()) throw new AssertionError();
-		if (service.getOwner() != this.getBorrower()) throw new AssertionError();
+		if (service.getOwner() != this.getOwner()) throw new AssertionError();
 		this.ownerServices.remove(service.getID());
 		this.notifyObservers();
 	}
