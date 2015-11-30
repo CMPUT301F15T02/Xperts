5515dd6ff493a273e9c737240493a37ab0855a2d
David Skrundz <david.skrundz@gmail.com>
2015-11-30 12:28:07 -0700

    image tests work

--- XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/UnitTests/PhotographTest.java
+++ XpertsApp/app/src/androidTest/java/ca/ualberta/cs/xpertsapp/UnitTests/PhotographTest.java
@@ -37,15 +37,16 @@ public class PhotographTest extends TestCase {
 		smallImage = BitmapFactory.decodeByteArray(smallByteArray_2, 0, smallByteArray_2.length);
 		assertTrue(smallImage.sameAs(BitmapFactory.decodeByteArray(smallByteArray_2, 0, smallByteArray_2.length)));
 
-//		String largeB64_1 = MyApplication.getContext().getString(R.string.largeTestImage);
-//		byte[] largeByteArray_1 = Base64.decode(largeB64_1, Base64.DEFAULT);
-//		Bitmap largeImage_1 = BitmapFactory.decodeByteArray(largeByteArray_1, 0, largeByteArray_1.length);
-//		ByteArrayOutputStream byteArrayOutputStream__1 = new ByteArrayOutputStream();
-//		largeImage_1.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream__1);
-//		byte[] b__1 = byteArrayOutputStream__1.toByteArray();
-//		String largeB64_2 = Base64.encodeToString(b__1, 2);
-//		byte[] smallByteArray__1 = Base64.decode(largeB64_2, Base64.DEFAULT);
-//		largeImage = BitmapFactory.decodeByteArray(smallByteArray__1, 0, smallByteArray__1.length);
+		String largeB64_1 = MyApplication.getContext().getString(R.string.largeTestImage);
+		byte[] largeByteArray_1 = Base64.decode(largeB64_1, Base64.DEFAULT);
+		Bitmap largeImage_1 = BitmapFactory.decodeByteArray(largeByteArray_1, 0, largeByteArray_1.length);
+		ByteArrayOutputStream byteArrayOutputStream1 = new ByteArrayOutputStream();
+		largeImage_1.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream1);
+		byte[] b_2 = byteArrayOutputStream1.toByteArray();
+		String largeB64_2 = Base64.encodeToString(b_2, 2);
+		byte[] largeByteArray_2 = Base64.decode(largeB64_2, Base64.DEFAULT);
+		largeImage = BitmapFactory.decodeByteArray(largeByteArray_2, 0, largeByteArray_2.length);
+		assertTrue(largeImage.sameAs(BitmapFactory.decodeByteArray(largeByteArray_2, 0, largeByteArray_2.length)));
 	}
 
 	@Override
@@ -53,6 +54,7 @@ public class PhotographTest extends TestCase {
 		super.tearDown2();
 	}
 
+	// Also 06.02.01
 	public void test_06_01_01() {
 		setUp2();
 		// Test attach photo
@@ -65,40 +67,44 @@ public class PhotographTest extends TestCase {
 		assertEquals(service.getPictures().size(), 1);
 		assertEquals(service.getPictures().get(0).hashCode(), smallImage.hashCode());
 
+		// Test view photo
 		ImageManager.sharedManager().clearCache();
 		assertTrue(service.getPictures().get(0).sameAs(smallImage));
 		tearDown2();
 	}
 
-	public void test_06_02_01() {
-		setUp2();
-		// TODO: Test view photo
-		assertTrue(false);
-
-		tearDown2();
-	}
-
 	public void test_06_03_01() {
 		setUp2();
-		// TODO: Test delete photo
-		assertTrue(false);
+		// Test delete photo
+		User user = UserManager.sharedManager().localUser();
+		Service service = ServiceManager.sharedManager().newService();
+		service.setName("testImageService");
+		user.addService(service);
+		service.addPicture(smallImage);
+
+		assertEquals(service.getPictures().size(), 1);
+		assertEquals(service.getPictures().get(0).hashCode(), smallImage.hashCode());
+
+		service.removePicture(0);
+		assertEquals(service.getPictures().size(), 0);
 
 		tearDown2();
 	}
 
 	public void test_06_04_01() {
 		setUp2();
-		// TODO: Test image size check under 65536 bytes
-		assertTrue(false);
+		// Test image size check under 65536 bytes
+		User user = UserManager.sharedManager().localUser();
+		Service service = ServiceManager.sharedManager().newService();
+		service.setName("testImageService");
+		user.addService(service);
+		service.addPicture(largeImage);
+
+		assertEquals(service.getPictures().size(), 1);
+		assertFalse(service.getPictures().get(0).sameAs(largeImage));
 
 		tearDown2();
 	}
 
-	public void test_06_05_01() {
-		setUp2();
-		// TODO: Test manually download individual photos if auto-download is off
-		assertTrue(false);
-
-		tearDown2();
-	}
+	// 06.05.01
 }
